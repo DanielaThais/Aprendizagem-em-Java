51ba4b699d5b32acb6fa79c67ba3f0199f29b742
Daniela Thais <daniela.thais64@gmail.com>
2025-05-06 23:10:58 -0300

    Add files via upload

---
+++ Aprendizagem/Streams/src/Teste/DesafioMap.java
@@ -0,0 +1,20 @@
+package Teste;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DesafioMap {
+	
+	public static void main(String[] args) {
+	
+		List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7,8,9);
+		
+		/*
+		 * 1. Converter o número inteiro para string binária: 6=> "110"
+		 * 2. Inverter a string: "110" => "011"
+		 * 3. Converter a string binária de volta a inteiro: "011" => 3
+		 */
+		
+		System.out.println(nums);
+}
+}
